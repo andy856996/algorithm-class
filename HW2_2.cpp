4116e579244e6a5f0c3648c421180200eb27422c
DingZeAn <andy856996@gmail.com>
2022-09-17 15:52:35 +0800

    first commit

--- HW2_2.cpp
+++ HW2_2.cpp
@@ -20,11 +20,43 @@ void bubble_sort(int arr[], int len) {//泡沫排序法
                 arr[j + 1] = temp;
             }
 }
-int main()
+int main(void)
 {	
-	int arr[5] = {1,2,3,4,5};
-	bubble_sort(arr,sizeof(arr)/sizeof(arr[0]));
-	
+	int n = 0,p = 0;
+	while(cin>>n>>p){
+		
+		int board[n];
+		for(int i =0;i<n;i++){
+			cin>>board[i];
+		}
+		
+		int poster[p];
+		for(int i =0;i<p;i++){
+			cin>>poster[i];
+		}
+		
+		int Length_board  = sizeof(board)/sizeof(int);
+		bubble_sort(board,Length_board);//對木板高度順序做排序 
+		
+		//新增變數 board_tmp 和 board一樣 
+		int board_tmp[n];
+		for(int i = 0;i < n;i++){
+			board_tmp[i] = board[i];
+		}
+		//翻轉 board 由大到小 
+		for(int i = 0;i < n;i++){
+			board[i] = board_tmp[Length_board-i-1];
+		}
+		//海報寬加總 
+		int sum =0;
+		for(int i = 0 ;i < p;i++){
+			sum = sum + poster[i];
+		}
+		//計算海報張貼最大高度
+		int max_high = board[sum-1] ;
+		//輸出
+		cout<<max_high<<endl;
+	}
     return 0;
 }
 
