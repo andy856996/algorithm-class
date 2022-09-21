31d1e31940ac9ff1332ede5192ed91650ed03431
DingZeAn <andy856996@gmail.com>
2022-09-21 08:54:26 +0800

    first class commit

--- HW2_2.cpp
+++ HW2_2.cpp
@@ -47,28 +47,62 @@ int main(void)
 //		}
 		
 		int Length_board  = sizeof(board)/sizeof(int);
-		bubble_sort(board,Length_board);//對木板高度順序做排序 
-		
+		int Length_poster  = sizeof(poster)/sizeof(int);
 		//新增變數 board_tmp 和 board一樣 
 		int board_tmp[n];
 		for(int i = 0;i < n;i++){
 			board_tmp[i] = board[i];
 		}
+		bubble_sort(board_tmp,Length_board);//對木板高度順序做排序 
+		
+		int board_tmp1[n];
+		for(int i = 0;i < n;i++){
+			board_tmp1[i] = board_tmp[i];
+		}
+		
 		//翻轉 board 由大到小 
 		for(int i = 0;i < n;i++){
-			board[i] = board_tmp[Length_board-i-1];
+			board_tmp[i] = board_tmp1[Length_board-i-1];
 		}
-		
-		if(n==0||p==0){
-			cout<<0<<endl;
-			continue;
-		}
-		
+
 		//海報寬加總 
 		int sum =0;
 		for(int i = 0 ;i < p;i++){
 			sum = sum + poster[i];
 		}
+		
+		if(n==0 || p==0 || p>sum){
+			cout<<0<<endl;
+			continue;
+		}
+		
+		int index_count = 0;
+		for(int i = 0;i < Length_board;i++){
+			max = board_tmp[i];
+			if(i!=0){
+				if(max == board[i-1] || !(board_tmp[sum-1] >= max) ){
+					continue;
+				}
+			}
+			
+			index_count = 0;
+			while(1){
+				if(board[index_count] >= max){
+					for(int ii = 0;ii < Length_poster;ii++){
+						for(int jj = 0;jj < Length_poster[ii];jj++){
+							if(index_count)
+						}
+					}
+				}else{
+					index_count ++;
+				}
+			}
+			
+			
+		}
+		
+		
+		
 		//計算海報張貼最大高度
 		if(sum != 0 && sum-1<=Length_board){
 			int max_high = board[sum-1];
